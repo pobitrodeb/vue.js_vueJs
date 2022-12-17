1dd3d459e202c84837de95bfae625e452f3b6b8f
Pobitrodeb <pobitrodn@gmail.com>
2022-12-17 21:15:24 +0600

    Data and attribute binding

---
+++ 2-2/js/vue/app.js
@@ -0,0 +1,15 @@
+var app = Vue.createApp({
+    data(){
+        return {
+            message : "wellcme to ", 
+
+            info: {
+                name: "Pobitro Debnath", 
+                webiste: "http://www.pobitrodeb.com/", 
+                gihub: "https://github.com/pobitrodeb"
+            } 
+        }
+    }
+});
+
+app.mount("#app");
