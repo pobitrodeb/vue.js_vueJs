021f9a2db55507af3a514acc87725e354cf4bab7
Pobitrodeb <pobitrodn@gmail.com>
2022-12-24 21:26:55 +0600

    normal attribute binding in 4_1

--- 3-1/js/vue/app.js
+++ 3-1/js/vue/app.js
@@ -8,10 +8,10 @@ var app = Vue.createApp({
     }, 
 
     methods: {
-        handleInput(e){
-         //   console.log(e);
-            this.inputNumber = e.target.value; 
-        }, 
+        // handleInput(e){
+        //  //   console.log(e);
+        //     this.inputNumber = e.target.value; 
+        // }, 
         getDubble(){
             this.result = this.inputNumber * 2 ;
         }, 
---
+++ 4_1_Class_binding/js/vue/app.js
@@ -0,0 +1,16 @@
+var app = Vue.createApp({
+    data(){
+        return {
+            activeClass:"red"
+        };
+
+    }, 
+
+    methods: {
+        setClass(className){
+            this.activeClass = className;
+        }
+    },
+});
+
+app.mount("#app");
