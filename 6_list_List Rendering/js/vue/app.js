6757ec63a09cf44a0e5c9cc965493635ca2d61b7
Pobitrodeb <pobitrodn@gmail.com>
2023-01-05 20:59:41 +0600

    List Rendering

---
+++ 6_list_List Rendering/js/vue/app.js
@@ -0,0 +1,14 @@
+var app = Vue.createApp({
+    data(){
+        return {
+            skills: ['html', 'css', 'js', 'php', 'mysql', 'oop']
+        };
+
+    }, 
+
+    methods: {
+        
+    },
+});
+
+app.mount("#app");
