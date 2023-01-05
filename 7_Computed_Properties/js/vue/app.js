3f749dfc0c56a1e0b3a50211d11dfd6f264b13bb
Pobitrodeb <pobitrodn@gmail.com>
2023-01-05 23:39:56 +0600

    remove list

--- 7_Computed_Properties/js/vue/app.js
+++ 7_Computed_Properties/js/vue/app.js
@@ -1,13 +1,21 @@
 var app = Vue.createApp({
     data(){
         return {
-            
+            skills: [
+                {name: 'HTML', project: 15},
+                {name: 'CSS', project: 5},
+                {name: 'JavaScript', project: 5},
+                {name: 'PHP', project: 10},
+                {name: 'Laravel', project: 18},
+            ]
         };
 
     }, 
 
     methods: {
-      
+        removeList(index){
+            this.skills.splice(index, 1);
+        }
     },
 });
 
