555841d5128a8dcf81f71ae0ee5f6067f693776f
Pobitrodeb <pobitrodn@gmail.com>
2023-01-05 21:07:59 +0600

    new list iteam add

--- 6_list_List Rendering/js/vue/app.js
+++ 6_list_List Rendering/js/vue/app.js
@@ -1,13 +1,16 @@
 var app = Vue.createApp({
     data(){
         return {
-            skills: ['html', 'css', 'js', 'php', 'mysql', 'oop']
+            skills: ['html', 'css', 'js', 'php', 'mysql', 'oop'], 
+            newSkill: ""
         };
 
     }, 
 
     methods: {
-        
+        addSkill(){
+            this.skills.push(this.newSkill);
+        }
     },
 });
 
