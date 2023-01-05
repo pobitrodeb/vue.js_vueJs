0944a1f6e73a051f9dfbe483f715d1668a58bb73
Pobitrodeb <pobitrodn@gmail.com>
2023-01-05 21:51:56 +0600

    catch object data

--- 6_list_List Rendering/js/vue/app.js
+++ 6_list_List Rendering/js/vue/app.js
@@ -1,8 +1,22 @@
 var app = Vue.createApp({
     data(){
         return {
-            skills: ['html', 'css', 'js', 'php', 'mysql', 'oop'], 
-            newSkill: ""
+            // skills: ['html', 'css', 'js', 'php', 'mysql', 'oop'], 
+            // array data 
+            skills: [
+                {name: 'HTML', project_exprience:35},
+                {name: 'CSS', project_exprience:25},
+                {name: 'BOOTSTRAP', project_exprience:22},
+                {name: 'RESPONSIVE DESIGN', project_exprience:18},
+                {name: 'JAVASCRIPT', project_exprience:15},
+                {name: 'VUE JS', project_exprience:25},
+                {name: 'jQuery', project_exprience:28},
+                {name: 'PHP', project_exprience:15},
+                {name: 'MySQl', project_exprience:15},
+                {name: 'OOP', project_exprience:15},
+                {name: 'LARAVEL', project_exprience:18},
+            ],
+            newSkill: "",
         };
 
     }, 
@@ -10,6 +24,7 @@ var app = Vue.createApp({
     methods: {
         addSkill(){
             this.skills.push(this.newSkill);
+            // this.newskill = " ";
         }, 
         removeSkill(index){
             this.skills.splice(index, 1);
