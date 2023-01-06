ad8cf440aab417b5dc199ae3b1a4239080f81fb6
Pobitrodeb <pobitrodn@gmail.com>
2023-01-06 12:53:04 +0600

    Watcher session successfully complete

--- 8_Watcher/js/vue/app.js
+++ 8_Watcher/js/vue/app.js
@@ -1,15 +1,18 @@
 var app = Vue.createApp({
     data(){
         return {
-            mobile: ""
+            mobile: "", 
+            name: {
+                firstName:"", 
+                lastName: "", 
+            }
         }
 
     }, 
 
 
     watch: {
-        mobile (newValue, oldValue)
-        {
+        mobile (newValue, oldValue){
             console.log(newValue);
             if(isNaN(newValue)){
                 alert('Enter Number no text');
@@ -19,7 +22,23 @@ var app = Vue.createApp({
             if(newValue.length == 11){
                 alert ("OTP Send Send Successfull"); 
             }
+        }, 
+
+        // specific vabe porperti er object dhora 
+        // 'name.firstName': function(newValue, oldValue){
+        //     console.log(newValue);
+        // },
+        // 'name.lastName': function(newValue, oldValue){
+        //     console.log(newValue);
+        // },
+// full poperties dhora 
+        name:{
+            deep: true, 
+            handler: function(newValue, oldValue){
+                console.log(newValue);
+            }
         }
+
     }
 });
 
