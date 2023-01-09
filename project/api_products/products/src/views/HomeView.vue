cee7d3e861bbb154ad0e114c3d456ed416443d20
Pobitrodeb <pobitrodn@gmail.com>
2023-01-09 22:05:20 +0600

    successfully show products home  page

--- project/api_products/products/src/views/HomeView.vue
+++ project/api_products/products/src/views/HomeView.vue
@@ -1,12 +1,24 @@
 <template>
-  <div></div>
+ <div class="card">
+  <div class="products" v-for="product in products" :key="product.id">
+   <img :src="product.image" alt="">
+    <h4> {{ product.title }}</h4>
+    <p>{{ product.price }}</p>
+  </div>
+ </div>
 </template>
 
 <script>
 import axios from 'axios';
 export default {
+  data(){
+    return {
+      product: [],
+    };
+  },
  mounted(){
  axios.get('https://fakestoreapi.com/products').then(res => {
+  this.products = res.data;
   console.log(res); 
  })
  .catch((e) => { 
@@ -16,4 +28,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.card{
+  display: flex;
+  justify-content: space-around;
+}
+.card img{
+    width: 70%;
+}
+</style>
