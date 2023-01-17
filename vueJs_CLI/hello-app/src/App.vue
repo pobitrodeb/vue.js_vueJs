ec7919edcbe64c369b32aae4ae00a69eb6ca55fb
Pobitrodeb <pobitrodn@gmail.com>
2023-01-17 22:16:05 +0600

    Recreating vue app with CLI seession

---
+++ vueJs_CLI/hello-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>{message}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "This data form App dot vue",
+    };
+  },
+};
+</script>
--- vueJs_CLI/hello-app/src/main.js
+++ vueJs_CLI/hello-app/src/main.js
@@ -1,24 +1,20 @@
 import { createApp } from "vue";
-// import App from "./App.vue";
+import App from "./App.vue";
 
 // createApp(App).mount("#app");
 // // console.log("hello app")
 // console.log("hello");
 
-var app = createApp({
-  data() {
-    return {
-      message: "This data form mainJs",
-    };
-  },
-  render() {
-    return "Hello World";
-  },
-  template: `
-    <div>
-        <h1> {{ message }} </h1>
-    </div>
-  `,
-});
+// var app = createApp({
+//   data() {
+//     return {
+//       message: "This data form mainJs",
+//     };
+//   },
+//   render() {
+//     return "Hello World";
+//   },
+// });
+var app = createApp(App);
 
 app.mount("#app");
