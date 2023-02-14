db7511c1b83ab2d6c5385c8596a1a26a31ea8159
Pobitrodeb <pobitrodn@gmail.com>
2023-02-14 21:15:24 +0600

    file added

--- project/htmlTemplete-mastering/src/main.js
+++ project/htmlTemplete-mastering/src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
